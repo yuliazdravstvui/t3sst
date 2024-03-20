737c6fe25176872c785aaf37edfb99ce18f05f81
gr421_koyuko <topolya.krutaya@gmail.com>
2024-03-20 16:29:34 +0700

    готово!

---
+++ cypress/e2e/spec-copy-1.cy.js
@@ -0,0 +1,6 @@
+cy.get('.header-container__desktop > .notification-bell > .notification-bell__icon__desktop > svg > path').click()
+
+cy.get('.header-container__desktop > .notification-bell > .notification-bell__icon__desktop > svg > path').click()
+
+
+cy.get('.header-container__desktop > .notification-bell > .notification-bell__similar > .notifications-similar > .search-title').should('exist')
--- cypress/e2e/spec4.cy.js
+++ cypress/e2e/spec4.cy.js
@@ -6,8 +6,8 @@ describe('template spec', () => {
     cy.visit('https://dev.profteam.su/')
     cy.get('[href="/registration"] > .button').click()
 
-    cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('studentysa')
-    cy.get('.form-input--email').type('studentysa@gmail.com')
+    cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('studentysaaa')
+    cy.get('.form-input--email').type('studentysaaa@gmail.com')
     cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type('Rfrfirf2004')
     cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type('Rfrfirf2004')
     cy.get(':nth-child(4) > .button').click()
